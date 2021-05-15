66a29924ee8d260dfe986c7098197b63d9e2c6ca
NganVanLuyen <nganvanluyen@gmail.com>
2021-05-15 23:04:30 +0700

    middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -36,22 +36,9 @@ module.exports.get = function (req, res) {
 
 module.exports.postCreate = function (req, res) {
    req.body.id = nanoid(idlength);
-   var errors = [];
-   if (!req.body.name) {
-      errors.push('Name is required!')
-   }
-
-   if (!req.body.phone) {
-      errors.push('Phone is required!')
-   }
-
-   if (errors.length) {
-      res.render('users/create', {
-         errors: errors, 
-         values: req.body
-      });
-      return;
-   }
+   
+   console.log(res.locals);
+   //có thể lưu biến để gửi cho middleware tiếp
 
    db.get('users').push(req.body).write();
    res.redirect('/users');
